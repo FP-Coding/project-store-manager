70b6a3054481a61c40d41d5b040b0af006faeb36
Felipe Pinto <felipe14062000@gmail.com>
2023-01-13 17:02:17 -0300

    feat: criação da função getById

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,11 +1,20 @@
 const { productModels } = require('../models');
 const { validateId } = require('./validations/validationInputValues');
 
-const getAll = () => {
-  const result = productModels.getAll();
+const getAll = async () => {
+  const result = await productModels.getAll();
+  return { type: null, message: result };
+};
+
+const getById = async (id) => {
+  const error = validateId(id);
+  if (error.type) return error;
+  const result = await productModels.getById();
+  if (!result) return { type: 'ID_NOT_FOUND', message: '"id" not found' };
   return { type: null, message: result };
 };
 
 module.exports = {
   getAll,
+  getById,
 };
