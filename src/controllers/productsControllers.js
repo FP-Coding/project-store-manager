115d252ffee5ccc4369ef786e215aae5d70a0562
Felipe Pinto <felipe14062000@gmail.com>
2023-01-13 17:39:07 -0300

    feat: criação da função controller getById

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,10 +1,20 @@
 const { productsService } = require('../services');
+const { errorMap } = require('../utils/mapError');
 
 const getAll = async (_req, res) => {
-  const result = await productsService.getAll();
-  return res.status(200).json(result.message);
+  const { type, message } = await productsService.getAll();
+  if (type) return res.status(400).json({ message });
+  return res.status(200).json(message);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productsService.getById(id);
+  if (type) return res.status(errorMap(type)).json({ message });
+  return res.status(200).json(message);
 };
 
 module.exports = {
   getAll,
+  getById,
 };
