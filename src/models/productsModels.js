f905e9331b235930c157bb26289cfd136a93aca7
Felipe Pinto <felipe14062000@gmail.com>
2023-01-13 16:04:07 -0300

    feat: implementação da função getById do ProductModels

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -1,12 +1,18 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute(
-    'SELECT * from StoreManager.products',
-  );
+  const query = 'SELECT * from StoreManager.products';
+  const [result] = await connection.execute(query);
+  return result;
+};
+
+const getById = async (productId) => {
+  const query = 'SELECT * from StoreManager.products WHERE id = ?';
+  const [[result]] = await connection.execute(query, [productId]);
   return result;
 };
 
 module.exports = {
   getAll,
+  getById,
 };
