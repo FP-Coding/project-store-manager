7c3cef976d4fd95cebcc07a5296bbfda44806ba4
Felipe Pinto <felipe14062000@gmail.com>
2023-01-13 19:01:42 -0300

    feat: criação dos testes da ProductServices

--- src/services/validations/validationInputValues.js
+++ src/services/validations/validationInputValues.js
@@ -2,7 +2,7 @@ const { idSchema } = require('./schema');
 
 const validateId = (id) => {
   const { error } = idSchema.validate(id);
-  if (error) return { type: 'INVALID_VALUE', message: error.message };
+  if (error) return { type: 'INVALID_VALUE', message: '"id" must be a number' };
   return { type: null, message: '' };
 };
 
---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+
+// const validationInputValues = require('../../../src/services/validations/validationInputValues')
+const { productModels } = require('../../../src/models')
+const { happyQueryAll, happyQueryById } = require('./mocks/productsServices.mock')
+const { productsService } = require('../../../src/services');
+
+describe('Testando service de Products', function () {
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  it('Testando getAll da productsServices', async function () {
+    // Arrange
+    sinon.stub(productModels, 'getAll').resolves(happyQueryAll)
+    // Act
+    const result = await productsService.getAll()
+    // Assert
+    expect(result.type).to.be.equal(null);
+    expect(result.message).to.be.deep.equal(happyQueryAll)
+  })
+
+  it('Testando getById da productsServices com id válido', async function () {
+    // Arrange
+    sinon.stub(productModels, 'getById').resolves(happyQueryById)
+    // Act
+    const result = await productsService.getById(1)
+    // Assert
+    expect(result.type).to.be.equal(null);
+    expect(result.message).to.be.deep.equal(happyQueryAll)
+  })
+
+  it('Testando getById da productsServices passando um id de um produto não existente', async function () {
+    // Arrange
+    sinon.stub(productModels, 'getById').resolves(undefined)
+    // Act
+    const result = await productsService.getById(4)
+    // Assert
+    expect(result.type).to.be.equal('PRODUCT_NOT_FOUND');
+    expect(result.message).to.be.deep.equal('Product not found')
+  })
+
+  it('Testando getById da productsServices passando um id invalido', async function () {
+    // Arrange
+    sinon.stub(productModels, 'getById').resolves(undefined)
+    // Act
+    const result = await productsService.getById('a')
+    // Assert
+    expect(result.type).to.be.equal('INVALID_VALUE');
+    expect(result.message).to.be.deep.equal('"id" must be a number')
+  })
+})
