2be63edd4781b9b8411ece15d2277d5f62976f2f
Felipe Pinto <felipe14062000@gmail.com>
2023-01-14 18:21:27 -0300

    feat: criação da rota post de /products e criação da camada função create da camada controller

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -13,7 +13,15 @@ const getById = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const create = async (req, res) => {
+  const { name } = req.body;
+  const { type, message } = await productsService.create(name);
+  if (type) return res.status(errorMap(type)).json({ message });
+  return res.status(201).json(message);
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
 };
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const { productsController } = require('../controllers');
+const { nameValidate } = require('../middlewares');
 
 const route = Router();
 
 route.get('/', productsController.getAll);
 
 route.get('/:id', productsController.getById);
+
+route.post('/', nameValidate, productsController.create);
 
 module.exports = route;
