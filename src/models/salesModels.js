6e75942c1b757f4833e7a06a1bb04000a5caf01f
Felipe Pinto <felipe14062000@gmail.com>
2023-01-15 00:39:05 -0300

    feat: criação da função getByIds do model de sales

--- src/models/salesModels.js
+++ src/models/salesModels.js
@@ -6,13 +6,21 @@ const create = async (sales) => {
   const placeholders = Object.keys(sales).map((_) => '?').join(', ');
   const query = `INSERT INTO StoreManager.sales_products (${columns}) VALUES (${placeholders})`;
   const salesInserted = sales.map((sale) => {
-    const [{ insertId }] = connection.execute(query, [...sale]);
+    const [{ insertId }] = connection.execute(query, [...Object.values(sale)]);
     return insertId;
   });
   await Promise.all(salesInserted);
   return salesInserted;
 };
 
+const getByIds = async (ids) => {
+  const placeholders = ids.map((_) => '?').join(', ');
+  const query = `SELECT * FROM StorageManager.sales_products WHERE sale_id IN (${placeholders})`;
+  const [result] = connection.execute(query, [...ids]);
+  return result;
+};
+
 module.exports = {
   create,
+  getByIds,
 };
